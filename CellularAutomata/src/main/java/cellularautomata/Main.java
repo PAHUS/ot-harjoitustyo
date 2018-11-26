6d58a76ab867f0fdd523c957275992a75977cfa8
Tande <taneli.vaananen@gmail.com>
2018-11-26 17:21:57 +0200

    GUI created

--- CellularAutomata/src/main/java/cellularautomata/Main.java
+++ CellularAutomata/src/main/java/cellularautomata/Main.java
@@ -1,38 +1,43 @@
 
 package cellularautomata;
 
-import java.util.Arrays;
+import cellularautomata.gui.GUI;
+import javafx.application.Application;
 
 public class Main {
     public static void main(String[] args) {
-        Grid grid = new Grid(20,20);
-        Rules rules = new GameOfLifeRules();
-        GameLogic logic = new GameLogic(grid, rules);
-        System.out.println("Hellou");
+        GameLogic logic = initializeDefault();
         
+        Application gui = new GUI();
+        gui.launch(GUI.class);
+        //käynnistetään GUI
         
-        Grid grid3 = new Grid(new boolean[][] {{false, true, true, false},
-                                             {false, true, false, false},
-                                             {false, true, false, false},
-                                             {false, true, false, false}});
-        
-        GameLogic gl = new GameLogic(grid3, rules);
-        for (int i =0; i<grid3.getxSize();i++){
-            for (int j = 0; j < grid3.getySize(); j++) {
-                int n = gl.aliveNeighbors(i, j);
-                System.out.println(n);
-            }
-        }
-        
-        System.out.println(Arrays.deepToString(grid3.getStates()));    
-        gl.iterate();
-        System.out.println(Arrays.deepToString(grid3.getStates()));   
-        
-        
+//        
+//        Grid grid3 = new Grid(new boolean[][] {{false, true, true, false},
+//                                             {false, true, false, false},
+//                                             {false, true, false, false},
+//                                             {false, true, false, false}});
+//        
+//        GameLogic gl = new GameLogic(grid3, rules);
+//        for (int i =0; i<grid3.getxSize();i++){
+//            for (int j = 0; j < grid3.getySize(); j++) {
+//                int n = gl.aliveNeighbors(i, j);
+//                System.out.println(n);
+//            }
+//        }
+//        
+//        System.out.println(Arrays.deepToString(grid3.getStates()));    
+//        gl.iterate();
+//        System.out.println(Arrays.deepToString(grid3.getStates()));   
+//        
         
     }
     
-    
-    
+    public static GameLogic initializeDefault(){
+        Grid grid = new Grid(50,50);
+        Rules rules = new GameOfLifeRules();
+        GameLogic logic = new GameLogic(grid, rules);
+        return logic;
+    } 
     
 }
